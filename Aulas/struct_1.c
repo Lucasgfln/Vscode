220b77f43578d67c89e658151d0e0ec000dc4ac5
Lucasgfln <lucasgabrielfln@gmail.com>
2025-07-07 17:01:57 -0300

    Corrige a leitura da quantidade de funcionários e ajusta a definição da estrutura Funcionario

--- Aulas/struct_1.c
+++ Aulas/struct_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Funcionario {
+struct Funcionario {
     int idade, rg;
     float salario;
 };
@@ -10,15 +10,26 @@ int main(void) {
     int n_funcionarios;
 
     printf(" * Informe a quantidade de funcionarios: ");
-    scanf("%i", n_funcionarios);
+    scanf("%i", &n_funcionarios);
 
     struct Funcionario funcionarios[n_funcionarios];
 
 
     if(n_funcionarios > 0) {
         for(int i = 0; i < n_funcionarios; i++) {
-            
+            printf(" * Rg: ");
+            scanf("%d", &funcionarios[i].rg);
+
+            printf(" * Idade: ");
+            scanf("%d", &funcionarios[i].idade);
+
+            printf(" * Salario: ");
+            scanf("%f", &funcionarios[i].salario);
+
+            printf("\n ######### Funcionario %i ######### \n => Rg: %i\n => Idade: %i\n => Salario: %.2f \n\n", i+1, funcionarios[i].rg, funcionarios[i].idade, funcionarios[i].salario);
         }
+
+        
 
     }
 
