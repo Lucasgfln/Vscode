00d6e53f125c2676d7e063219630ec3a46f71352
Lucasgfln <lucasgabrielfln@gmail.com>
2025-07-08 16:12:22 -0300

    Adiciona funções para cálculo de fatorial e para encontrar o maior valor em um vetor

--- Atividades/Lista de Atividade 3/q_10(Primos).c
+++ Atividades/Lista de Atividade 3/q_10(Primos).c
@@ -1,12 +1,7 @@
-#include <stdio.h>
-
 int main (void){
     printf("\n\n<=================== I N I C I O ===================>\n\n");
     
     
-
-    printf("\n\n<================ R E S U L T A D O ================>\n\n");
-
     printf("\n\n<==================== F I N A L ====================>\n\n");
     return 0;
 }
---
+++ Atividades/Sala/Atividades/funcoes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fatorialde(int a) {
+    if(a >= 0) {
+        int fatorial = 1;
+
+        for(int i = 1; i <= a; i++) {
+            fatorial *= i;
+        }
+
+        return fatorial;
+    } else {
+        printf(" ERRO: NAO EXISTE FATORIAL DE UM NUMERO MENOR QUE 0!!!");
+        return -1;
+    }
+}
+
+float maior(float *vetor, int tam){
+    float maior = vetor[0];
+
+    for (int i = 1; i < tam; i++) {
+        if(maior < vetor[i]) maior = vetor[i];
+    }
+
+    return maior;
+}
